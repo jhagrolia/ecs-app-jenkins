07c893f63754b2c80df525d9d475624cea0f196c
jhagrolia <aman.jhagrolia@tothenew.com>
2021-07-12 23:38:35 +0530

    ECS App

--- App/deploy-web.tf
+++ App/deploy-web.tf
@@ -72,7 +72,7 @@ resource "aws_security_group" "service_sg" {
   ingress {
     from_port = 80
     to_port = 80
-    protocol = "tcp"
+    protocol = "tcp" 
     security_groups = [aws_security_group.alb_sg.id]
   }
 
